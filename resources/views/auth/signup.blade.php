27b725eb1abc6c8a3c06d1861247336210833008
anestiandinesta <anestiandi05@gmail.com>
2025-03-27 23:29:45 +0700

    Anestaday1

---
+++ resources/views/auth/signup.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+</head>
+<body>
+    <h2>Signup</h2>
+    <form action="{{ route('signup.submit') }}" method="POST">
+        @csrf
+        <label>Name:</label>
+        <input type="text" name="name" required><br>
+
+        <label>Email:</label>
+        <input type="email" name="email" required><br>
+
+        <label>Password:</label>
+        <input type="password" name="password" required><br>
+
+        <label>Confirm Password:</label>
+        <input type="password" name="password_confirmation" required><br>
+
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
